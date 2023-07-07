042da28b846fa1311a3a5a4ae8dc9d4b7a193e79
zen <zenon.deviagge@gmail.com>
2023-07-07 09:03:51 -0300

    readBook function added to Book class. Tested successfully

--- script.js
+++ script.js
@@ -8,32 +8,31 @@ class Book {
     this.read = read;
   };
 
-
+  readBook = function (element) {
+    const chkbox = document.createElement('input');
+    chkbox.setAttribute('type', 'checkbox');
+    chkbox.setAttribute('id', 'innerchkbox');
+  
+    const lbl = document.createElement('label');
+    lbl.setAttribute('for', 'innerchkbox');
+  
+    lbl.appendChild(document.createTextNode('Read '));
+  
+    const chkBoxDiv = document.createElement('div');
+    chkBoxDiv.setAttribute('class', 'card-label');
+    chkBoxDiv.appendChild(lbl);
+    chkBoxDiv.appendChild(chkbox);
+  
+    element.appendChild(chkBoxDiv);
+  
+    if (this.read === true) {
+      chkbox.checked = true;
+    } else {
+      chkbox.checked = false;
+    }
+  };
 }
 
-Book.prototype.readBook = function (element) {
-  const chkbox = document.createElement('input');
-  chkbox.setAttribute('type', 'checkbox');
-  chkbox.setAttribute('id', 'innerchkbox');
-
-  const lbl = document.createElement('label');
-  lbl.setAttribute('for', 'innerchkbox');
-
-  lbl.appendChild(document.createTextNode('Read '));
-
-  const chkBoxDiv = document.createElement('div');
-  chkBoxDiv.setAttribute('class', 'card-label');
-  chkBoxDiv.appendChild(lbl);
-  chkBoxDiv.appendChild(chkbox);
-
-  element.appendChild(chkBoxDiv);
-
-  if (this.read === true) {
-    chkbox.checked = true;
-  } else {
-    chkbox.checked = false;
-  }
-};
 
 function addBookToLibrary() {
   const bookAuthor = document.getElementById('author').value;
