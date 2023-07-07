a6dfee7d691d6ca1c864163d036e21cbfb507288
zen <zenon.deviagge@gmail.com>
2023-07-07 08:59:23 -0300

    class and constructor refactor

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const myLibrary = [];
 
-function Book(author, title, pages, read) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor (author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+  };
+
+
 }
 
 Book.prototype.readBook = function (element) {
